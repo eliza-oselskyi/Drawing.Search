30d8e57e5c1e651f6006d01b6498271e7ad189d3
Ellie Oselskyi <ellie.oselskyi@kirbybuildingsystems.com>
2025-05-08 14:14:40 -0500

    initial commit

---
+++ Drawing.Search.Core/SearchManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Tekla.Structures;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+using ModelObject = Tekla.Structures.Model.ModelObject;
+using Part = Tekla.Structures.Drawing.Part;
+using Task = System.Threading.Tasks.Task;
+
+namespace Drawing.Search.Core
+{
+    public class SearchManager
+    {
+        private readonly DrawingHandler  _drawingHandler = new DrawingHandler();
+        private readonly Model _model = new Model();
+
+        public SearchManager()
+        {
+        }
+
+        public async Task<bool>  ExecuteSearch(string query)
+        {
+            var drawing = _drawingHandler.GetActiveDrawing();
+            var idList = _drawingHandler.GetModelObjectIdentifiers(_drawingHandler.GetActiveDrawing());
+            var objList = _model.FetchModelObjects(idList, false);
+            var modelObjects = await Task.Run(() => GetMatchedModelObjects(query, objList));
+            if (modelObjects.Count == 0) return false;
+            
+            var selector = _drawingHandler.GetDrawingObjectSelector();
+            var doArrayList = new ArrayList();
+
+            foreach (var x in modelObjects.Select(o => drawing.GetSheet().GetModelObjects(o.Identifier)))
+            {
+                while (x.MoveNext())
+                {
+                    if (x.Current != null) doArrayList.Add(x.Current);
+                }
+            }
+            
+            selector.SelectObjects(doArrayList, false);
+            return true;
+        }
+
+        private static List<ModelObject> GetMatchedModelObjects(string query, List<ModelObject> objList)
+        {
+            return (objList.FindAll((m) =>
+            {
+                var prop = string.Empty;
+                m.GetReportProperty($"ASSEMBLY_POS", ref prop);
+
+                if (prop == string.Empty)
+                {
+                    m.GetReportProperty($"PART_POS", ref prop);
+                }
+
+                if (!(m is Beam beam)) return prop.Equals(query);
+                return beam.GetAssembly().GetMainObject().Equals(m) && prop.Equals(query);
+            }));
+        }
+    }
+}
---
+++ Drawing.Search/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Drawing.Search.Core;
+
+namespace Drawing.Search
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var searchManager = new SearchManager();
+            var text = SelectTextBox.Text;
+            
+            searchManager.ExecuteSearch(text);
+        }
+    }
+}
