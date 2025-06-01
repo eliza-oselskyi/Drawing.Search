c537fdb52ff9422641ab881df61c59aeb10085c3
belajaakacija <belajaakacija@pm.me>
2025-05-31 23:45:05 -0500

    doc(searchservice): add documentation

--- Drawing.Search.Core/SearchService/SearchService.cs
+++ Drawing.Search.Core/SearchService/SearchService.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using Drawing.Search.Core.CADIntegrationService.Interfaces;
+using Drawing.Search.Core.SearchService.Interfaces;
 
 namespace Drawing.Search.Core.SearchService;
 
 public class SearchService
 {
     private readonly IDrawingHandler _drawingHandler;
+    private static readonly Lazy<ISearchLogger> LoggerInstance = new Lazy<ISearchLogger>(() => new SearchLogger());
+    
+    public static ISearchLogger GetLoggerInstance() => LoggerInstance.Value;
 
     public SearchService(IDrawingHandler drawingHandler)
     {
