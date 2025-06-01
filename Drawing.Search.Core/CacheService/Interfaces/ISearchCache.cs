71f6b8845db1acb29e9fffbf339a8e7f16b1cf0f
belajaakacija <belajaakacija@pm.me>
2025-05-31 22:48:31 -0500

    feat: add cache service

---
+++ Drawing.Search.Core/CacheService/Interfaces/ISearchCache.cs
@@ -0,0 +1,9 @@
+﻿namespace Drawing.Search.Core.CacheService.Interfaces;
+
+public interface ISearchCache
+{
+    void RefreshCache();
+    void ClearCache();
+    void AddMainKeyToCache(string mainKey);
+    void AddEntryByMainKey(string mainKey, string entryKey, object entryValue);
+}
