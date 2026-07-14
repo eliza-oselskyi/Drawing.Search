7df11acf3d36fe2220e3935787741d7703d7be46
Eliza Pavlovna Oselskaya <ellie.oselskyi@kirbybuildingsystems.com>
2026-07-14 16:36:21 -0500

    feat: add search pipeline

---
+++ Drawing.Search.Application/Features/Search/SearchPipeline.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Drawing.Search.Domain.Interfaces;
+
+namespace Drawing.Search.Application.Features.Search;
+
+public static class SearchPipeline
+{
+    public static IReadOnlyList<SearchMatch<T>> Search<T>(
+        IEnumerable<T> items,
+        Func<T, string> extract,
+        ISearchQuery query,
+        SearchPredicate predicate)
+    {
+        if (items is null) throw new ArgumentNullException(nameof(items));
+        if (extract is null) throw new ArgumentNullException(nameof(extract));
+        if (query is null) throw new ArgumentNullException(nameof(query));
+        if (predicate is null) throw new ArgumentNullException(nameof(predicate));
+
+        return items
+            .Where(item => item is not null)
+            .Select(item => new SearchMatch<T>(item, extract(item) ?? string.Empty))
+            .Where(match => predicate(match.Content, query))
+            .ToList();
+    }
+}
